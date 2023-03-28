a10b003f7560aac6a417d5079f95c05d85f89151
胡静 <810898541@qq.com>
2023-03-28 15:14:56 +0800

    feat: table precision

--- packages/ui/src/Form/src/components/Table.vue
+++ packages/ui/src/Form/src/components/Table.vue
@@ -37,7 +37,7 @@
             v-model:value="record[column.dataIndex]"
             :min="column.min"
             :max="column.max"
-            :precision="column.precision || 2"
+            :precision="column.precision ?? 2"
           />
 
           <Input v-else v-model:value="record[column.dataIndex]" />
