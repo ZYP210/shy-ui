2b2b764e7af2416ef6bc60595d10d3d17474781b
zephyr <zephyrljy@gmail.com>
2023-05-16 10:34:23 +0800

    1

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
         '@shy-plugins/utils',
         '@zxcvbn-ts/core',
         'sortablejs',
-        'lodash-es',
         'virtual:svg-icons-names',
         '@logicflow/core',
         '@purge-icons/generated',
