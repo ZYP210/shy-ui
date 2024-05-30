eaf685bf50e5c8e9c5460e32a917a4db1c36a2cc
胡静 <810898541@qq.com>
2024-05-30 10:19:48 +0800

    fix: 更新bpmn dingding vite config

--- packages/workflow-dingding/vite.config.ts
+++ packages/workflow-dingding/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     //cssCodeSplit: true,
     rollupOptions: {
       //忽略打包vue文件
-      external: ['vue','ant-design-vue'],
+      external: ['vue', '3h1-ui', '@shy-plugins/use', 'ant-design-vue'],
       input: ['./index.ts'],
       output: [
         {
