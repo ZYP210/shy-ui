aa617fc239b690a83dd4f3283a3cef8463463bc2
sunlei <sunlei@3h1china.com>
2024-08-29 16:58:34 +0800

    feat: 3.0.0-next.145 * 抛出表格表单旧版别名

--- packages/ui/src/ShyTable/index.ts
+++ packages/ui/src/ShyTable/index.ts
@@ -1,5 +1,5 @@
 import ShyTable from './src/ShyTable'
-export { default as ShyTableAction } from './src/components/ShyTableAction'
+export { default as ShyTableAction, default as TableAction } from './src/components/ShyTableAction'
 export { shyTableActionProps } from './src/components/ShyTableAction'
 import type { App, Plugin } from 'vue'
 import { basicPropChange } from './src/props'
