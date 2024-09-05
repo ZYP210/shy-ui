9e139e615283b7d5b75218c496a2b12fafaf9917
zephyr <zephyrljy@gmail.com>
2024-09-05 15:21:55 +0800

    feat: 添加Description 函数

--- packages/ui/src/Description/index.ts
+++ packages/ui/src/Description/index.ts
@@ -4,3 +4,4 @@ import { basicProps } from './src/props'
 export * from './src/typing'
 export { useDescription } from './src/useDescription'
 export { Description, basicProps as descriptionProps }
+export { add as addDescriptionComponent } from './src/ShyComponentMap'
