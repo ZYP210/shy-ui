2b139cd0ce1de7b1b3ba6bcf710d89aa1a37e23f
zephyr <zephyrljy@gmail.com>
2024-12-04 09:58:48 +0800

    feat: 添加inputTextarea

--- packages/ui/src/ShyForm/src/components/FormItem.tsx
+++ packages/ui/src/ShyForm/src/components/FormItem.tsx
@@ -95,7 +95,10 @@ const FormItem = defineComponent({
         }
       }
 
-      if (schema.component === 'Input') {
+      if (
+        schema.component === 'Input' ||
+        schema.component === 'InputTextArea'
+      ) {
         const maxlength =
           componentProps?.maxlength === undefined
             ? 100
